fa916f9813da5a1c6c3fffdf8f598ef974ad8a4a
Alex Balabon <balanon7@gmail.com>
2019-05-15 13:04:23 +0300

    Adds project EvoMobileNotes

---
+++ EvoMobileNotes/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  EvoMobileNotes
+//
+//  Created by mac on 5/15/19.
+//  Copyright © 2019 sashabalabon. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ EvoMobileNotes/NotesListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotesListViewController.swift
+//  EvoMobileNotes
+//
+//  Created by mac on 5/15/19.
+//  Copyright © 2019 sashabalabon. All rights reserved.
+//
+
+import UIKit
+
+class NotesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
