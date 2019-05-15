111998035e3b03ed786561c2b3baf0616912a921
Alex Balabon <balanon7@gmail.com>
2019-05-15 13:54:33 +0300

    delete and save data of notes

--- EvoMobileNotes/NotesListViewController.swift
+++ EvoMobileNotes/NotesListViewController.swift
@@ -23,17 +23,28 @@ class NotesListViewController: UIViewController, UITableViewDataSource  {
         
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewNote))
         self.navigationItem.rightBarButtonItem = addButton
+        loadingData()
     }
     
     @objc func addNewNote(){
         let name = "Item \(textData.count + 1)"
         textData.insert(name, at: 0)
+        
         let indexPath = IndexPath(row: 0, section: 0)
         noteTableView.insertRows(at: [indexPath], with: .automatic)
+        
+        saveData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return textData.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        textData.remove(at: indexPath.row)
+        noteTableView.deleteRows(at: [indexPath], with: .fade)
+        
+        saveData()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,6 +53,18 @@ class NotesListViewController: UIViewController, UITableViewDataSource  {
         
         return cell!
     }
+    
+    func saveData() {
+        UserDefaults.standard.set(textData, forKey: "text")
+    }
 
+    func loadingData() {
+        if let loadedData = UserDefaults.standard.value(forKey: "text") {
+            textData = loadedData as! [String]
+            noteTableView.reloadData()
+        }
+        
+    }
 
+    
 }
