c7b6ca6ac0c140a15ad4a5d1a755e3ef9f7dc252
Alex Balabon <balanon7@gmail.com>
2019-05-15 13:38:46 +0300

    Add new botton and cells

--- EvoMobileNotes/NotesListViewController.swift
+++ EvoMobileNotes/NotesListViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class NotesListViewController: UIViewController {
+class NotesListViewController: UIViewController, UITableViewDataSource  {
 
+
+    @IBOutlet weak var noteTableView: UITableView!
+    
+    var textData = ["Item 1", "Item 2", "Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        noteTableView.dataSource = self
+        self.title = "List of notes"
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewNote))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func addNewNote(){
+        let name = "Item \(textData.count + 1)"
+        textData.insert(name, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        noteTableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = textData[indexPath.row]
+        
+        return cell!
     }
 
 
